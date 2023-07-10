8623fc4ab69e0752a916d603ac03f7df3104c1ae
Alon L <alon@botanica.consulting>
2023-07-10 15:22:55 +0300

    tests and workflow

---
+++ wiredialer_test.go
@@ -0,0 +1,28 @@
+package wiredialer
+
+import (
+    "testing"
+    "strings"
+)
+
+const VALID_CONFIG = `
+[Interface]
+Address = 4.3.2.1/32,3.2.1.0/16
+PrivateKey = QvKsOZ9oQvBs5n79sOXWh4QnxLrIh5Ii34H3w/1mfGQ=
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = QvKsOZ9oQvBs5n79sOXWh4QnxLrIh5Ii34H3w/1mfGQ=
+AllowedIPs = 0.0.0.0/0,::/0
+Endpoint = 1.2.3.4:1234
+`
+
+func TestNewFromConfig(t *testing.T) {
+    reader := strings.NewReader(VALID_CONFIG)
+    _, err := NewDialerFromConfiguration(reader)
+    if err != nil {
+        t.Error(err)
+    }
+}
+
+
